1d4046deb73dd6892edb8be9d9240aeffc479fa8
Willian Galvani <williangalvani@gmail.com>
2020-04-17 17:17:26 -0300

    Make camera point at rov without following it

---
+++ scripts/InterpolatedCamera.gd
@@ -0,0 +1,16 @@
+extends InterpolatedCamera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.look_at($"/root/Node2D/ReferenceRect/ViewportCamera/Viewport/BlueRov".transform.origin, Vector3(0,1,0))
