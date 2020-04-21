4247e6939e6af7ab3fafab9816fd253332e365cf
Willian Galvani <williangalvani@gmail.com>
2020-04-20 22:05:33 -0300

    Add fog only underwater

--- scripts/teste.gd
+++ scripts/teste.gd
@@ -3,6 +3,8 @@ extends Spatial
 const BUOYANCY = 160.0 # newtons?
 const HEIGHT = 2.4 # TODO: get this programatically
 
+var underwater_env = load("res://underwaterEnvironment.tres")
+var surface_env = load("res://default_env.tres")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
@@ -18,7 +20,19 @@ func calculate_buoyancy():
 		var buoyancy =  min(vehicle.buoyancy, abs(vehicle.buoyancy*(vehicle.translation.y - HEIGHT/2 - surface_altitude)))
 		vehicle.add_force(Vector3(0, buoyancy, 0), vehicle.transform.basis.y*0.07)
 	
+func update_fog():
+	var vehicles = get_tree().get_nodes_in_group("vehicles")
+	for vehicle in vehicles:
+		if not vehicle is RigidBody:
+			push_warning("Component %s does not inherit RigidBody." % vehicle.name)
+			continue
+
+		var surface_altitude = $water.translation.y
+		var camera = get_node(str(vehicle.get_path()) + "/Camera")
+		var depth = camera.global_transform.origin.y - surface_altitude
+		camera.environment = surface_env if depth > 0 else underwater_env
+
 
 func _physics_process(_delta):
 	calculate_buoyancy()
-	
+	update_fog()
