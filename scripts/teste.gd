47c3688739c4023c5a1ed4e038a0765bf275eaac
Willian Galvani <williangalvani@gmail.com>
2020-04-20 23:25:05 -0300

    Add underside of water and fog scaling

--- scripts/teste.gd
+++ scripts/teste.gd
@@ -5,6 +5,8 @@ const HEIGHT = 2.4 # TODO: get this programatically
 
 var underwater_env = load("res://underwaterEnvironment.tres")
 var surface_env = load("res://default_env.tres")
+# darkest it gets
+var deep_color = underwater_env.background_color
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
@@ -31,6 +33,13 @@ func update_fog():
 		var camera = get_node(str(vehicle.get_path()) + "/Camera")
 		var depth = camera.global_transform.origin.y - surface_altitude
 		camera.environment = surface_env if depth > 0 else underwater_env
+		var fog_distance = max(50 + 3 * depth, 10)
+		underwater_env.fog_depth_end = fog_distance
+		var new_color = surface_env.background_sky.sky_top_color.linear_interpolate(Color("05517b"), 1-(fog_distance/55))
+		underwater_env.background_color = new_color
+		underwater_env.fog_color = new_color
+		underwater_env.ambient_light_color = new_color
+
 
 
 func _physics_process(_delta):
