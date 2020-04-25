de11d11ea14f1f3d30fb119321a0565f042a5b42
Willian Galvani <williangalvani@gmail.com>
2020-04-25 13:48:16 -0300

    tweak underwater environment

--- scripts/teste.gd
+++ scripts/teste.gd
@@ -33,12 +33,13 @@ func update_fog():
 		var camera = get_node(str(vehicle.get_path()) + "/Camera")
 		var depth = camera.global_transform.origin.y - surface_altitude
 		camera.environment = surface_env if depth > 0 else underwater_env
-		var fog_distance = max(50 + 3 * depth, 10)
+		var fog_distance = max(50 + 3 * depth, 20)
 		underwater_env.fog_depth_end = fog_distance
-		var new_color = surface_env.background_sky.sky_top_color.linear_interpolate(Color("05517b"), 1-(fog_distance/55))
+		var new_color = Color("28a4e9").linear_interpolate(Color("001e5f"), 1-(fog_distance/55))
 		underwater_env.background_color = new_color
 		underwater_env.fog_color = new_color
-		underwater_env.ambient_light_color = new_color
+		underwater_env.ambient_light_energy = fog_distance/100
+		underwater_env.ambient_light_color = Color("a5d6f1").linear_interpolate(Color("001e5f"), 1-(fog_distance/50))
 
 
 
