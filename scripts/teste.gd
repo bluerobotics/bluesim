2c353482758f13ffa51a1f4cb67333e88b498729
Willian Galvani <williangalvani@gmail.com>
2020-04-25 22:30:23 -0300

    make external camera follow the rov, enable environment for it

--- scripts/teste.gd
+++ scripts/teste.gd
@@ -7,6 +7,9 @@ var underwater_env = load("res://underwaterEnvironment.tres")
 var surface_env = load("res://default_env.tres")
 # darkest it gets
 var deep_color = underwater_env.background_color
+onready var cameras = get_tree().get_nodes_in_group("cameras")
+onready var surface_altitude = $water.translation.y
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
@@ -18,7 +21,6 @@ func calculate_buoyancy():
 			push_warning("Component %s does not inherit RigidBody." % vehicle.name)
 			continue
 
-		var surface_altitude = $water.translation.y
 		var buoyancy =  min(vehicle.buoyancy, abs(vehicle.buoyancy*(vehicle.translation.y - HEIGHT/2 - surface_altitude)))
 		vehicle.add_force(Vector3(0, buoyancy, 0), vehicle.transform.basis.y*0.07)
 	
@@ -28,11 +30,8 @@ func update_fog():
 		if not vehicle is RigidBody:
 			push_warning("Component %s does not inherit RigidBody." % vehicle.name)
 			continue
-
-		var surface_altitude = $water.translation.y
-		var camera = get_node(str(vehicle.get_path()) + "/Camera")
-		var depth = camera.global_transform.origin.y - surface_altitude
-		camera.environment = surface_env if depth > 0 else underwater_env
+		var rov_camera = get_node(str(vehicle.get_path()) + "/Camera")
+		var depth = rov_camera.global_transform.origin.y - surface_altitude
 		var fog_distance = max(50 + 3 * depth, 20)
 		underwater_env.fog_depth_end = fog_distance
 		var new_color = Color("28a4e9").linear_interpolate(Color("001e5f"), 1-(fog_distance/55))
@@ -41,7 +40,8 @@ func update_fog():
 		underwater_env.ambient_light_energy = fog_distance/100
 		underwater_env.ambient_light_color = Color("a5d6f1").linear_interpolate(Color("001e5f"), 1-(fog_distance/50))
 
-
+		for camera in cameras:
+			camera.environment = surface_env if depth > 0 else underwater_env
 
 func _physics_process(_delta):
 	calculate_buoyancy()
