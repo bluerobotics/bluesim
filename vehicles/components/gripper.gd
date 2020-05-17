dd7ffc3f4a201d95742913dbaee3c912925a3619
Willian Galvani <williangalvani@gmail.com>
2020-05-17 15:36:26 -0300

    Adding gripper to bluerov heavy

--- vehicles/components/gripper.gd
+++ vehicles/components/gripper.gd
@@ -5,10 +5,6 @@ const speed = 0.4
 
 func _ready():
 	set_physics_process(true)
-	$Area.contact_monitor = true
-	$Area.contacts_reported = 1
-	$Area.connect("body_entered", self, "_contact")
-	$Area.connect("body_exited", self, "_exit_contact")
 
 func _contact(node):
 	print("inside", node)
@@ -17,26 +13,23 @@ func _exit_contact(node):
 	print("outside", node)
 
 func _physics_process(delta):
-	if $"g1".rotation_degrees.z < 20 and direction > 0 \
-		or $"g1".rotation_degrees.z > -6 and direction < 0:
-		$"g1".rotate_z(direction * speed * delta)
-		$"g2".rotate_z(-direction * speed * delta)
-		
+	if Input.is_action_pressed("gripper_open"):
+		print("opening")
+		open()
+	elif Input.is_action_pressed("gripper_close"):
+		close()
+	else:
+		stop()
+
+	
 func open():
-	direction = -1
+	$"../../joint3".set_param(6, -1.0)
+	$"../../joint4".set_param(6, 1.0)
 	
 func close():
-	direction = 1
-
+	$"../../joint3".set_param(6, 1.0)
+	$"../../joint4".set_param(6, -1.0)
+	
 func stop():
-	direction = 0
-
-"""
-func _input(event):
-	if event.is_action_type():
-		match event.as_text():
-			"Up":
-				direction = -1 if event.is_pressed() else 0
-			"Down":
-				direction = 1 if event.is_pressed() else 0
-"""
+	$"../../joint3".set_param(6, 0.0)
+	$"../../joint4".set_param(6, 0.0)
