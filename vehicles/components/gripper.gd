97ba99f7319f4569f481c33c70c55a14bd3e91ab
Willian Galvani <williangalvani@gmail.com>
2020-05-20 16:15:33 -0300

    fix gui.gd for removed gripper

--- vehicles/components/gripper.gd
+++ vehicles/components/gripper.gd
@@ -12,24 +12,3 @@ func _contact(node):
 func _exit_contact(node):
 	print("outside", node)
 
-func _physics_process(delta):
-	if Input.is_action_pressed("gripper_open"):
-		print("opening")
-		open()
-	elif Input.is_action_pressed("gripper_close"):
-		close()
-	else:
-		stop()
-
-	
-func open():
-	$"../../joint3".set_param(6, -1.0)
-	$"../../joint4".set_param(6, 1.0)
-	
-func close():
-	$"../../joint3".set_param(6, 1.0)
-	$"../../joint4".set_param(6, -1.0)
-	
-func stop():
-	$"../../joint3".set_param(6, 0.0)
-	$"../../joint4".set_param(6, 0.0)
