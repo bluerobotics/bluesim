3a529595936dc0091314f0118ff7ba75b901ab5a
Willian Galvani <williangalvani@gmail.com>
2020-07-18 16:04:58 -0300

    preparing for multiple vehicles

--- scripts/targetPos.gd
+++ scripts/targetPos.gd
@@ -1,15 +1,14 @@
 extends Spatial
 
-onready var rov = get_parent().find_node("cameraTarget", true, false)
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	
-	var distance = (self.global_transform.origin - rov.global_transform.origin)
+	var vehicle = Globals.active_vehicle.find_node("cameraTarget", true, false)
+	var distance = (self.global_transform.origin - vehicle.global_transform.origin)
 	var length = distance.length()
 	if length > 3:
-		self.global_transform.origin =  rov.global_transform.origin + distance * 3/length
-	self.look_at(rov.global_transform.origin, Vector3.UP)
+		self.global_transform.origin =  vehicle.global_transform.origin + distance * 3/length
+	self.look_at(vehicle.global_transform.origin, Vector3.UP)
