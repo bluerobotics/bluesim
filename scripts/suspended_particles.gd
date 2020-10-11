3303776256346690d34f894f9b3eccf8b544f05e
Patrick José Pereira <patrickelectric@gmail.com>
2020-10-10 22:08:47 -0300

    scripts: Check if there is a vehicle before accessing it
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- scripts/suspended_particles.gd
+++ scripts/suspended_particles.gd
@@ -17,5 +17,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if not self.vehicle:
+		return
 	global_transform.origin = vehicle.global_transform.origin
 	global_transform.basis = vehicle.global_transform.basis
